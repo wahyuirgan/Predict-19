d90f8ee9f50d560348074e1da396782b73b6bbc7
wahyuirgan12 <wahyuirgan12@gmail.com>
2021-05-27 22:33:57 +0700

    Temporarily Remove Koin, Repository, Utilities and Extensions.

--- app/src/main/java/academy/bangkit/predict19/ui/RegisterActivity.kt
+++ app/src/main/java/academy/bangkit/predict19/ui/RegisterActivity.kt
@@ -2,17 +2,14 @@ package academy.bangkit.predict19.ui
 
 import academy.bangkit.predict19.databinding.ActivityRegisterBinding
 import academy.bangkit.predict19.ui.login.LoginActivity
-import academy.bangkit.predict19.ui.login.LoginViewModel
 import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
-import org.koin.android.ext.android.inject
+import android.widget.Toast
 
 class RegisterActivity : AppCompatActivity() {
 
     private lateinit var binding: ActivityRegisterBinding
-
-    private val loginViewModel: LoginViewModel by inject()
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -20,7 +17,9 @@ class RegisterActivity : AppCompatActivity() {
         setContentView(binding.root)
 
         binding.btnRegister.setOnClickListener {
-
+            val intent = Intent(this@RegisterActivity, LoginActivity::class.java)
+            startActivity(intent)
+            Toast.makeText(this, "Registered", Toast.LENGTH_SHORT).show()
         }
 
         binding.tvHaveAccount.setOnClickListener {
